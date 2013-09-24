a0945c646fddcccad23766913881f22c247a433a
DaJoker29 <dj729@comcast.net>
2013-09-23 19:47:56 -0700

    Switched to PHP instead of static HTML for interface

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<?php include 'php/variables.php'; ?>
+
+<html>
+    <head>
+        <title><?php echo TITLE;?></title>
+        <meta charset="utf-8" />
+        <link href="stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+        <link href="stylesheets/print.css" media="print" rel="stylesheet" type="text/css" />
+        <!--[if IE]>
+            <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+        <![endif]-->
+    </head>
+
+    <body>
+    </body>
+</html>
