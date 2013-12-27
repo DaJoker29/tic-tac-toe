f379ed57a1e4fed78300fbe9887ce54d0d1b7b36
DaJoker29 <dj729@comcast.net>
2013-12-27 14:46:13 -0800

    move ariadne and implement into index

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <head>
         <title>Ariadne - Tic Tac Toe bot</title>
         <meta charset="utf-8" />
+        <script src="js/ariadne.js"></script>
     </head>
 
     <body>
@@ -11,6 +12,7 @@
                 <h1 id="title">Ariadne</h1>
                 <p id="description">Tic Tac Toe Bot</p>
             </header>
+            <?php require("form.php"); ?>
         </div>
     </body>
 </html>
